3e129f82ad00c6268ba3c259ff0f58bed67a3c82
EugeneShatalin <shatalin_ei@mail.ru>
2021-07-30 01:10:56 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
-}
+    let arr = expr.split('');
+    let leftParenthesis = 0, rightParenthesis = 0;
 
-module.exports = {
-    expressionCalculator
-}
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] == '0' && arr[i-2] == '/') {
+            throw Error("TypeError: Division by zero.");
+        }пше
+
+        if(arr[i] == '(') {
+            leftParenthesis += 1;
+        }
+        if(arr[i] == ')') {
+            rightParenthesis += 1;
+        }
+    }
+
+   if(leftParenthesis != rightParenthesis) {
+    throw Error("ExpressionError: Brackets must be paired");
+   }
+
+   return (new Function('return ' + expr))();
+
+  }
+
+  module.exports = {
+    expressionCalculator,
+  };
